86829c2f94a73bd6f8c0231cb6ce30ef006b2010
tinysheep007 <pzxiaomie@gmail.com>
2022-08-15 17:33:14 -0400

    added uuid

--- src/Components/TwoImagesCompare.js
+++ src/Components/TwoImagesCompare.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import axios from 'axios';
 import { Button } from "@mui/material";
+import * as uuid from 'uuid';
 
 export default function TwoImagesCompare() {
 
@@ -11,6 +12,9 @@ export default function TwoImagesCompare() {
     const [file2, setFile2] = useState();
     const [pic2base64, setPic2base64] = useState();
     const [result, setResult] = useState();
+    const [sampleUUID, setSampleUUID] = useState();
+    const [id1, setId1] = useState(1);
+    const [id2, setId2] = useState(2);
 
     const onChangePic1 = (event) => {
         if (event.target.files && event.target.files[0]) {
@@ -31,7 +35,7 @@ export default function TwoImagesCompare() {
             const myArray = ans.split("base64,");
             setPic1base64(myArray[1])
             console.log(myArray[1])
-            
+
         };
 
         reader.onerror = function (error) {
@@ -65,8 +69,8 @@ export default function TwoImagesCompare() {
             console.log('Error: ', error);
         };
     }
-    
-   
+
+
     const handleSubmission = () => {
         axios({
             method: 'post',
@@ -74,15 +78,21 @@ export default function TwoImagesCompare() {
             headers: {},
             data: {
                 pic1base64: pic1base64,
-                pic1ID:"1",
+                pic1ID: id1,
                 pic2base64: pic2base64,
-                pic2ID:"2"
+                pic2ID: id2
             }
         }).then((res) => {
             //please wait for the windows prompt for guidance 
             alert("File Upload success");
+            setResult(res)
         })
             .catch((err) => alert("File Upload Error"));;
+    }
+
+    const generateUUID = () => {
+        setId1(uuid.v4);
+        setId2(uuid.v4)
     }
 
     return (
@@ -107,25 +117,40 @@ export default function TwoImagesCompare() {
                     For API parameter check TwoIamgesCompare.js
                 </p>
 
+                <p>
+                    Remeber to click the genarate a new UUID for both photo before submitting
+                </p>
+
             </div>
-            <div style={{width:"50%",float:"left"}}>
-                Image 1 Upload
+            <div style={{ width: "50%", float: "left" }}>
+                <div style={{ font: "30px", fontWeight: "bold" }}>
+                    Image 1 Upload
+                </div>
+
+
                 <div>
                     <input type="file" onChange={onChangePic1} ></input>
                 </div>
-                <img src={pic1} style={{ border:"5px solid red", width:"300px", height:"300px" }}/>
+                <img src={pic1} style={{ border: "5px solid red", width: "300px", height: "300px" }} />
 
             </div>
 
             <div>
-                Image 2 Upload
+                <div style={{ font: "30px", fontWeight: "bold" }}>
+                    Image 2 Upload
+                </div>
                 <div>
                     <input type="file" onChange={onChangePic2}></input>
                 </div>
-                <img src={pic2} style={{ border:"5px solid pink", width:"300px", height:"300px" }}/>
+                <img src={pic2} style={{ border: "5px solid pink", width: "300px", height: "300px" }} />
             </div>
 
-            
+            <Button variant="contained" onClick={generateUUID}>display UUID </Button>
+            <div>
+                <div>UUID FOR PIC1 : {id1}</div>
+                <div>UUID FOR PIC2 : {id2}</div>
+            </div>
+
             <Button variant="contained" color="success" onClick={handleSubmission}>Compare Two Images</Button>
             <div>
                 display results below
