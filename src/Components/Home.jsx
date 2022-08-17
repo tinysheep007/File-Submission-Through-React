e4d6df07f45927fc8b5b1e85d600c15a065ec063
tinysheep007 <pzxiaomie@gmail.com>
2022-08-17 17:44:58 -0400

    more style

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,11 +1,14 @@
 import React from "react";
-import rigLOGO from '../rigLOGO.jpg';
+import rigLOGO from "../rigLOGO.jpg";
+import { Button } from "@mui/material";
 
 export default function Home() {
   return (
     <div>
       <h1>This is Home Page!</h1>
-      <div style={{fontSize:"30px"}}>Go Explore By pressing the navbar tabs!</div>
+      <div style={{ fontSize: "30px" }}>
+        Go Explore By pressing the navbar tabs!
+      </div>
       <p>
         First, go through the Submit tab. You will first learn the basic through
         converting photos into base64 format using NPM modules.
@@ -23,8 +26,12 @@ export default function Home() {
         to fully understand
         https://github.com/tinysheep007/File-Submission-Through-React
       </p>
-      <img src={rigLOGO}/>
-
+      <Button href="/Submit" variant="contained" style={{padding:"15px", fontSize:"15px", marginRight:"20px"}}>Don't Know Where to Start?</Button>
+      
+      <Button href="https://github.com/tinysheep007/RIG-Photo-Submission-Comparasion-Through-Base64" variant="outlined" style={{padding:"15px", fontSize:"15px"}}>Go to Code!</Button>
+      <div>
+        <img src={rigLOGO} />
+      </div>
     </div>
   );
 }
